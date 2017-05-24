0fc324cd11d1b323764dc7caf62acd6bfb3177a2
Yaroslav Sapelyuk <yaroslav.sapelyuk@gmail.com>
2017-05-24 13:25:42 +0300

    Start project
    Adding main function

---
+++ CanonizePolinom.py
@@ -0,0 +1,12 @@
+#! Reduction of a polynomial to a canonical form
+
+def main():
+    print("Main function")
+    poly = str(input("Enter your polynom: "))
+    print("\nYour polynom is: ")
+    for char in poly:
+        print(char, end="")
+    print("\n")
+
+if __name__ =="__main__":
+    main()
